dbf2b0b09352f43566603a8568e848f91f65a5b9
raushanraie01 <raushanraie01@gmail.com>
2025-08-26 21:27:47 +0530

    contact page delete

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import About from "./components/About";
 import Resume from "./components/Resume";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
       <Resume />
       <Projects />
       <Skills />
-      {/* <Contact /> */}
       <Footer />
     </div>
   );
